7ea9fd9ee4aa73db3c77dbd978c5bcce7266f4c2
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-05-27 22:52:14 +0300

    Add files via upload

---
+++ SquareOfStarsOtherSolution/SquareOfStars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareofStars
+{
+    class SquareOfStars
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            StringBuilder strBeg = new StringBuilder(new string('*', a));
+            Console.WriteLine(strBeg.ToString());
+            for (int i = 0; i < a - 2; i++)
+            {
+                StringBuilder strMiddle = new StringBuilder(new string(' ', a - 2));
+                Console.Write("*");
+                Console.Write(strMiddle.ToString());
+                Console.WriteLine("*"); // To Ask
+            }
+            StringBuilder strEnd = new StringBuilder(new string('*', a));
+
+            Console.WriteLine(strEnd.ToString());
+        }
+    }
+}

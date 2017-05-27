bcc7ab97668b931f4b95fab9610b38e6cea1fcea
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-05-27 19:33:43 +0300

    Add files via upload

---
+++ Square Of Stars/SquareOfStars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStarsSoftUni
+{
+    class SquareOfStars
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', a));
+            for (int i = 1; i <= a - 2; i++)
+                Console.WriteLine("*" + new string(' ', a - 2) + "*");
+            Console.WriteLine(new string('*', a));         
+        }
+    }
+}

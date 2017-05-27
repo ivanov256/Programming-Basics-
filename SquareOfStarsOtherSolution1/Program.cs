4ab994e9c67fd2a1612a6acfafdbd66795313b73
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-05-27 22:54:52 +0300

    Add files via upload

---
+++ SquareOfStarsOtherSolution1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6_SquareOfStarsOtherSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            int i, k;
+
+            for (i = 0; i < a; i++) //We draw the top of the square
+            {
+                Console.Write("*");
+            }
+            for (i = 0; i < a - 2; i++) //Determines what to do on each row
+            {
+                Console.WriteLine();
+                Console.Write("*");
+
+                for (k = 0; k < a - 2; k++) //Draws the inner of the square
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("*");
+            }
+            Console.WriteLine(); //We draw the bottom of the square
+            for (i = 0; i < a; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}

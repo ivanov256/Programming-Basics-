48c71ef63a9f5fc8c9b24dfcca500a1c680c8b06
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:14:23 +0300

    Add files via upload

---
+++ SimpleCalculations/SchoolCamp/SchoolLab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolCamp
+{
+    class SchoolLab
+    {
+        static void Main(string[] args)
+        {
+            double h = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+
+            var rows = (h * 100) / 120;
+            var cols = ((w * 100) - 100) / 70;
+            var places = Math.Floor(rows) * Math.Floor(cols) - 3;
+            Console.WriteLine(places);
+
+            
+        }
+    }
+}

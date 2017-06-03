c338e1d657da1fefd388f4abb2473b5d78ac52b8
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:08:12 +0300

    Add files via upload

---
+++ SimpleCalculations/ConsoleCurrencyConvertor/ConsoleCurrencyConvertor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCurrencyConvertor
+{
+    class ConsoleCurrencyConvertor
+    {
+        static void Main(string[] args)
+        {
+            decimal currencyAmount = decimal.Parse(Console.ReadLine());
+            string currencyInput = Console.ReadLine();
+            string currencyOutput = Console.ReadLine();
+
+            if (currencyInput == "BGN")
+            {
+                if (currencyOutput == "USD")
+                {
+                    currencyAmount /= 1.79549m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "EUR")
+                {
+                    currencyAmount /= 1.95583m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "GBP")
+                {
+                    currencyAmount /= 2.53405m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+            }
+
+            else if (currencyInput == "USD")
+            {
+                if (currencyOutput == "EUR")
+                {
+                    currencyAmount *= 1.79549m / 1.95583m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "GBP")
+                {
+                    currencyAmount *= 1.79549m / 2.53405m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "BGN")
+                {
+                    currencyAmount *= 1.79549m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+            }
+
+            else if (currencyInput == "EUR")
+            {
+                if (currencyOutput == "USD")
+                {
+                    currencyAmount *= 1.95583m / 1.79549m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "GBP")
+                {
+                    currencyAmount *= 1.95583m / 2.53405m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "BGN")
+                {
+                    currencyAmount *= 1.95583m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+            }
+
+            else if (currencyInput == "GBP")
+            {
+                if (currencyOutput == "EUR")
+                {
+                    currencyAmount *= 2.53405m / 1.95583m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "USD")
+                {
+                    currencyAmount *= 2.53405m / 1.79549m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+
+                else if (currencyOutput == "BGN")
+                {
+                    currencyAmount *= 2.53405m;
+                    Console.WriteLine(Math.Round(currencyAmount, 2));
+                }
+            }               
+        }
+    }
+}

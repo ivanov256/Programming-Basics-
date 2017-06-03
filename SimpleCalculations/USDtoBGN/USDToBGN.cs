c24cc225625a57e16c16cf120e804c82b3b40552
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:17:47 +0300

    Add files via upload

---
+++ SimpleCalculations/USDtoBGN/USDToBGN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDtoBGN
+{
+    class USDToBGN
+    {
+        static void Main(string[] args)
+        {
+            decimal usd = decimal.Parse(Console.ReadLine());
+            decimal bgn = usd * 1.79549m;
+            Console.WriteLine("{0:f2} BGN", bgn);
+        }
+    }
+}

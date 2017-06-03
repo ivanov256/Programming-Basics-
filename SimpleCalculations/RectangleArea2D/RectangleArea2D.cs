5823f4cb2c7014f0c050853831fc57e528afdb1b
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:13:23 +0300

    Add files via upload

---
+++ SimpleCalculations/RectangleArea2D/RectangleArea2D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea2D
+{
+    class RectangleArea2D
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double width = Math.Max(x1, x2) - Math.Min(x1, x2);
+            double height = Math.Max(y1, y2) - Math.Min(y1, y2);
+            double area = height * width;
+            double perimeter = 2 * (width + height);
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+
+        }
+    }
+}

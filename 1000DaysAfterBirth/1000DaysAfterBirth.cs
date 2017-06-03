14131b3ea735dafe775b0b4817c6d578fb45abcc
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-03 23:57:40 +0300

    Add files via upload

---
+++ 1000DaysAfterBirth/1000DaysAfterBirth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Globalization;
+
+namespace _1000DaysAfterBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string birthDate = Console.ReadLine();
+            //CultureInfo provider = CultureInfo.InvariantCulture;
+            DateTime result = DateTime.ParseExact(birthDate, "dd-MM-yyyy", null);
+            DateTime date = result.AddDays(999);
+            Console.WriteLine(date.ToString("dd-MM-yyyy"));
+            
+        }
+    }
+}

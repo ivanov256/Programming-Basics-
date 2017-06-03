d1e4c22acaef6caa2040870c02cfab7d41d67e50
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:13:56 +0300

    Add files via upload

---
+++ SimpleCalculations/RepairingTheTiles/RepairingTheTiles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairingTheTiles
+{
+    class RepairingTheTiles
+    {
+        static void Main(string[] args)
+        {
+            int playgroundLength = int.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
+            double tileLength = double.Parse(Console.ReadLine());
+            int benchWidth = int.Parse(Console.ReadLine());
+            int benchLength = int.Parse(Console.ReadLine());
+
+            int playgroundArea = playgroundLength * playgroundLength;
+            double tileArea = tileLength * tileWidth;
+            int benchArea = benchLength * benchWidth;
+
+            int repairingArea = playgroundArea - benchArea;
+            double tilesCounter = repairingArea / tileArea;
+            double repairTime = tilesCounter * 0.2;
+
+            Console.WriteLine("{0:f2}", tilesCounter);
+            Console.WriteLine("{0:f2}", repairTime);
+        }
+    }
+}

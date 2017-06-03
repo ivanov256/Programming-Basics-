eaec5db92174f4d2d64f8fcd6132494f805a424e
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:16:47 +0300

    Add files via upload

---
+++ SimpleCalculations/TriangleArea/TriangleArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea
+{
+    class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = (a * h) / 2;
+            Console.WriteLine(Math.Round(area, 2));
+            //Console.WriteLine("{0:f2}", area);
+        }
+    }
+}

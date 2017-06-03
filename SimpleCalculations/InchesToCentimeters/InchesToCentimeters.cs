7be0061387520734499d843af2db187285fe3726
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:10:37 +0300

    Add files via upload

---
+++ SimpleCalculations/InchesToCentimeters/InchesToCentimeters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchesToCentimeters
+{
+    class InchesToCentimeters
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inches = ");
+            double inches = double.Parse(Console.ReadLine());
+            var centimeters = inches * 2.54;
+            Console.Write("Centimeters = ");
+            Console.WriteLine(centimeters);
+        }
+    }
+}

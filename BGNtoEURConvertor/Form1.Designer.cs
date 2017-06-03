7170d1ef8150eeba3250890f7a56b8e4542fbf69
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:00:34 +0300

    Add files via upload

---
+++ BGNtoEURConvertor/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BGNtoEURConvertor
+{
+    partial class FormConvert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelChange = new System.Windows.Forms.Label();
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.labelBGNToEUR = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelChange
+            // 
+            this.labelChange.AutoSize = true;
+            this.labelChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelChange.Location = new System.Drawing.Point(30, 45);
+            this.labelChange.Name = "labelChange";
+            this.labelChange.Size = new System.Drawing.Size(64, 20);
+            this.labelChange.TabIndex = 0;
+            this.labelChange.Text = "Convert";
+            this.labelChange.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownAmount.Location = new System.Drawing.Point(120, 43);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(83, 26);
+            this.numericUpDownAmount.TabIndex = 1;
+            this.numericUpDownAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAmount.ValueChanged += new System.EventHandler(this.numericUpDownAmount_ValueChanged);
+            this.numericUpDownAmount.KeyUp += new System.Windows.Forms.KeyEventHandler(this.numericUpDownAmount_KeyUp);
+            // 
+            // labelBGNToEUR
+            // 
+            this.labelBGNToEUR.AutoSize = true;
+            this.labelBGNToEUR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBGNToEUR.Location = new System.Drawing.Point(228, 45);
+            this.labelBGNToEUR.Name = "labelBGNToEUR";
+            this.labelBGNToEUR.Size = new System.Drawing.Size(105, 20);
+            this.labelBGNToEUR.TabIndex = 2;
+            this.labelBGNToEUR.Text = "BGN To EUR";
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.Chartreuse;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(30, 90);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(303, 30);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(345, 139);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelBGNToEUR);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Controls.Add(this.labelChange);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormConvert";
+            this.ShowIcon = false;
+            this.Text = "BGN To EUR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelChange;
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.Label labelBGNToEUR;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ BGNtoEURConvertor/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGNtoEURConvertor
+{
+    public partial class FormConvert : Form
+    {
+        public FormConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Convert();
+        }
+
+        private void numericUpDownAmount_ValueChanged(object sender, EventArgs e)
+        {
+            Convert();
+        }
+
+        private void numericUpDownAmount_KeyUp(object sender, KeyEventArgs e)
+        {
+            Convert();
+        }
+        private void Convert()
+        {
+            decimal amountBGN = this.numericUpDownAmount.Value;
+            decimal amountEUR = amountBGN / 1.95583m;
+            this.labelResult.Text =
+                amountBGN + " BGN" + " = " + Math.Round(amountEUR, 2) + " EUR";
+
+        }
+    }
+}

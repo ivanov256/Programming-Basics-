e3402698c80cf866f54f773a9e1f609ffc2f0183
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:09:24 +0300

    Add files via upload

---
+++ SimpleCalculations/Greeting/Greeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    class Greeting
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            //Console.WriteLine("Hello " + name);
+            //Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}

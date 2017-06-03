87b190ce2da799d254f950fd1ab730dc16b96591
Hristo Hr. Ivanov <hristoivanov369@gmail.com>
2017-06-04 00:11:56 +0300

    Add files via upload

---
+++ SimpleCalculations/Profit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int workDays = int.Parse(Console.ReadLine());
+            float wage = float.Parse(Console.ReadLine());
+            float dollarRate = float.Parse(Console.ReadLine());
+
+            double monthlySalary = workDays * wage;
+            double bonus = monthlySalary * 2.5;
+            double annualSalary = monthlySalary * 12 + bonus;            
+            double incomeTaxes = annualSalary * 0.25;
+            double annualIncome = (annualSalary - incomeTaxes) * dollarRate;
+            double averageDayEarnings = annualIncome / 365;
+            Console.WriteLine("{0:f2}", averageDayEarnings);
+
+
+        }
+    }
+}
